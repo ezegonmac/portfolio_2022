c6c50f92959a366d88161e0445b8ae539c98b4a1
unknown <ezegonmac01@gmail.com>
2022-08-29 18:39:46 +0100

    Style projects section Cards

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -14,19 +14,38 @@ const Header = styled.span`
 
 const CardStyled = styled.div`
     background-color: pink;
-    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 
     & img {
         width: 100%;
+        max-height: 75%;
+    }
+
+    & p {
+        width: 100%;
+        text-align: end;
+        color: magenta;
+        margin: 0;
+        padding-right: 1.5em;
+        font-weight: 500;
+        font-size: 1.1rem;
+    }
+
+    &:hover {
+        transform: scale(1.03);
+        transition: 0.1s ease-in-out;
     }
 `
 
 const Card = ({ project }) => {
     return(
         <CardStyled>
-            <Link href={project.link}>
-                <img src={project.img}></img>
-            </Link>
+                <Link href={project.link}>
+                    <img src={project.img}></img>
+                </Link>
+                <p>{project.title}</p>
         </CardStyled>
     )
 }
