dfb6226cf9b2029944b0136e12b52d3c3a042c92
unknown <ezegonmac01@gmail.com>
2022-09-04 01:28:37 +0200

    Fix decoration overflow

--- components/Decorations.js
+++ components/Decorations.js
@@ -1,8 +1,19 @@
+import styled from "styled-components"
+
+const DecorationWrapper = styled.span`
+    overflow: hidden;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 34%;
+`
 
 export const MiddleWaveDecoration = () => {
     return(
-        <img 
-            src="/wave2.svg" 
-            style={{ position: "absolute", left: 0, top: "34%", opacity: "16%"}}/>
+        <DecorationWrapper>
+            <img 
+                src="/wave2.svg" 
+                style={{ opacity: "16%" }}/>
+        </DecorationWrapper>
     )
 }
