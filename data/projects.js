0f3c96d850fbd24729a4e633120bd84ca2114a6f
ezegonmac <ezegonmac01@gmail.com>
2023-01-08 19:55:22 +0100

    Add new projects

--- data/projects.js
+++ data/projects.js
@@ -6,14 +6,24 @@ const projects = [
       img: "/projects/portfolio.png"
     },
     {
+      title: "Cube Market",
+      link: "https://github.com/ezegonmac/CubeMarket",
+      img: "/projects/cubemarket.png"
+    },
+    {
+      title: "Innoweb",
+      link: "https://github.com/innosoft-innoweb/innosoft-innoweb-1",
+      img: "/projects/innoweb.png"
+    },
+    {
+      title: "DTechnology",
+      link: "https://github.com/ezegonmac/DTechnology",
+      img: "/projects/dtechnology.png"
+    },
+    {
       title: "Reactive TikTakToe",
       link: "https://ezegonmac.github.io/React-TikTakToe",
       img: "/projects/tiktaktoe.png"
-    },
-    {
-      title: "Mandelbulb",
-      link: "https://ezegonmac.github.io/3d-curves/",
-      img: "/projects/mandelbulb.png"
     },
     {
       title: "Weights",
@@ -26,20 +36,25 @@ const projects = [
       img: "/projects/gameoflife.png"
     },
     {
-      title: "React Gallery",
-      link: "https://github.com/ezegonmac/react-gallery/",
-      img: "/projects/reactgallery.png"
-    },
-    {
       title: "Seven Islands",
       link: "https://github.com/ezegonmac/dp1-2021-2022-l8-1/",
       img: "/projects/sevenislands.jpg"
     },
-    {
-      title: "IISSI2 Project",
-      link: "https://github.com/ezegonmac/Proyecto-curso-IISSI2/",
-      img: "/projects/iissi2.png"
-    },
+    // {
+    //   title: "Mandelbulb",
+    //   link: "https://ezegonmac.github.io/3d-curves/",
+    //   img: "/projects/mandelbulb.png"
+    // },
+    // {
+    //   title: "React Gallery",
+    //   link: "https://github.com/ezegonmac/react-gallery/",
+    //   img: "/projects/reactgallery.png"
+    // },
+    // {
+    //   title: "IISSI2 Project",
+    //   link: "https://github.com/ezegonmac/Proyecto-curso-IISSI2/",
+    //   img: "/projects/iissi2.png"
+    // },
 ]
 
 export default projects
